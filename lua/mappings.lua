6dcd0a83724e88702617e443b49d8fdb70cea512
bg <gennadyborisenko@gmail.com>
2025-03-06 23:46:17 +0300

    add plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -33,3 +33,10 @@ end, { desc = "Toogle Terminal Horizontal" })
 map("t", "<C-f>", function()
   require("nvchad.term").toggle { pos = "float" }
 end, { desc = "Toogle Terminal Float" })
+
+-- Git
+map("n", "<leader>gl", ":Flog<CR>", { desc = "Git Log" })
+map("n", "<leader>gg", ":LazyGit<CR>", { desc = "Git Log" })
+map("n", "<leader>gf", ":DiffviewFileHistory<CR>", { desc = "Git File History" })
+map("n", "<leader>gc", ":DiffviewOpen HEAD~1<CR>", { desc = "Git Last Commit" })
+-- map("n", "<leader>gt", ":DiffviewToggleFile<CR>", { desc = "Git File History" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -401,6 +401,88 @@ return {
       end
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    lazy = false,
+  },
+  { "sindrets/diffview.nvim", lazy = false },
+  -- git
+  { "tpope/vim-fugitive" },
+  { "rbong/vim-flog", dependencies = {
+    "tpope/vim-fugitive",
+  }, lazy = false },
+  -- красивое переименование
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+  -- {
+  --   "julienvincent/hunk.nvim",
+  --   cmd = { "DiffEditor" },
+  --   config = function()
+  --     require("hunk").setup()
+  --   end,
+  -- },
   -- {
   --   "jay-babu/mason-nvim-dap.nvim",
   --   dependencies = "mason.nvim",
